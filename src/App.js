f43bd48e99c1495fe811d7666c94daf9e0c8299c
kkwoju <kkwo4140@gmail.com>
2023-04-10 21:39:37 +0900

    숫자 카운터 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React, { useState } from "react";
 
 function App() {
+  // [초기값, 초기값을 어떻게 할 것인가]
+  const [num, setNum] = useState(0);
+
+  const onBtnIncreaseClicked = () => setNum(num + 1);
+
+  const onBtnDecreaseClicked = () => setNum(num - 1);
+  
+  const reset = () => {
+    setNum(0);
+  };
+
+
+
+
   return (
-   <div>안녕</div>
+    <>
+      <div>
+        NUM : {num}
+      </div>
+      <button onClick={onBtnIncreaseClicked}>1 증가</button>
+      <br/>
+      <button onClick={onBtnDecreaseClicked}>1 감소</button>
+      <br/>
+      <button onClick={() => setNum(num + 10)}>10 증가</button>
+      <br/>
+      <button onClick={() => setNum(num - 10)}>10 감소</button>
+      <br/>
+      <button onClick={reset}>Reset</button>
+    </>
   );
 }
 
