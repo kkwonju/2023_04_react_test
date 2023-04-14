688f00ffa618f93933091b2658a9fd85c1ce6899
kkwoju <kkwo4140@gmail.com>
2023-04-14 20:50:45 +0900

    재개

--- src/SetTimeOut.js
+++ src/SetTimeOut.js
@@ -6,11 +6,15 @@ export default function SetTimeOut() {
     const timeoutId = setTimeout(() => setNum(num + 1), 1000);
 
     const pause = () => clearTimeout(timeoutId);
+
+    const resume = () => {setTimeout(() => setNum(num + 1), 1000)};
     return (
         <>
             숫자 : {num}
             <hr />
             <button onClick={pause}>일시정지</button>
+            &nbsp;
+            <button onClick={resume}>재개</button>
         </>
     );
 }
