0f939e7faaf46b7f3ddcba1121e10ec8b06b0ca0
kkwoju <kkwo4140@gmail.com>
2023-04-17 20:50:20 +0900

    상품 리스트 제작 2, 컴포넌트 변수 활용

--- src/App.js
+++ src/App.js
@@ -4,7 +4,23 @@ export default function App() {
 
   return (
     <>
-      <ProductListItem />
+      <div style={{ display: 'flex', gap: '10px' }}>
+        <ProductListItem
+          imgNo={1}
+          name="Mac Book"
+          productPriceFormatted={'3,000,000'}
+        />
+        <ProductListItem
+          imgNo={2}
+          name="MacBook Pro"
+          productPriceFormatted={'3,500,000'}
+        />
+        <ProductListItem
+          imgNo={201}
+          name="Mac Book air"
+          productPriceFormatted={'4,000,000'}
+        />
+      </div>
     </>
   );
 }
--- src/ProductListItem.js
+++ src/ProductListItem.js
@@ -1,24 +1,20 @@
 import React, { useState } from "react";
 
-export default function ProductListItem() {
+export default function ProductListItem(props) {
+    const imgNo = props.imgNo;
+    const productName = props.name;
+    const productPriceFormatted = props.productPriceFormatted;
+
     return (
         <>
-            <div style={{display: "flex", gap: '10px'}}>
-                <div style={{ display: "inline-flex", flexDirection: "column", gap: "10px" }}>
-                    <img src="https://picsum.photos/id/250/200/300" alt="" />
-                    <div style={{ textAlign: 'center' }}>Nikon Camera</div>
-                    <div style={{ textAlign: 'center' }}>3,000,000</div>
-                </div>
-                <div style={{ display: "inline-flex", flexDirection: "column", gap: "10px" }}>
-                    <img src="https://picsum.photos/id/1/200/300" alt="" />
-                    <div style={{ textAlign: 'center' }}>Mac Book PRO</div>
-                    <div style={{ textAlign: 'center' }}>4,500,000</div>
-                </div>
-                <div style={{ display: "inline-flex", flexDirection: "column", gap: "10px" }}>
-                    <img src="https://picsum.photos/id/2/200/300" alt="" />
-                    <div style={{ textAlign: 'center' }}>Mac Book Pro Plus</div>
-                    <div style={{ textAlign: 'center' }}>5,000,000</div>
-                </div>
+            <div style={{
+                display: "inline-flex",
+                flexDirection: "column",
+                gap: "10px"
+            }}>
+                <img src={`https://picsum.photos/id/${imgNo}/400/400`} alt="" />
+                <div style={{ textAlign: 'center', color: 'gray' }}>{productName}</div>
+                <div style={{ textAlign: 'center', color: 'gray' }}>{productPriceFormatted}</div>
             </div>
         </>
     )
