a3d883b1b3c7b990c651f5956f93e2e183d8dff8
kkwoju <kkwo4140@gmail.com>
2023-04-19 20:02:41 +0900

    3가지 방법

--- src/App.js
+++ src/App.js
@@ -2,16 +2,9 @@ import NoRecord from "./NoRecord";
 
 export default function App() {
 
-  const nums = [1, 2, 3, 4, 5, 6, 7, 8];
-  const arr1 = nums.map((el) => <li>내용 {el}</li>)
-
-  const arr2 = [<li>내용1</li>, <li>내용2</li>, <li>내용3</li>];
   return (
     <>
       <NoRecord />
-      <hr />
-      <ul>{arr1}</ul>
-      <ul>{arr2}</ul>
     </>
   );
 }
--- src/NoRecord.js
+++ src/NoRecord.js
@@ -7,6 +7,8 @@ export default function NoRecord() {
   const saveNo = () => {
     setRecordedNos([...recordedNos, no]);
   };
+  // const li = [1,2,3].map((el, index) => <li key={index}>{el}</li>);
+  const li = recordedNos.map((el, index) => <li key={index}>{el},</li>);
 
   return (
     <>
@@ -15,8 +17,28 @@ export default function NoRecord() {
         value={no}
         onChange={(e) => setNo(e.target.valueAsNumber)} />
       <button type="button" onClick={saveNo}>기록</button>
+
+      {/* 버전 1 */}
       <hr />
+      <h1>기록된 숫자 v1</h1>
       기록된 숫자 : [{recordedNos.join(",")}]
+
+      {/* 버전 2 */}
+      <hr />
+      <h1>기록된 숫자 v2_1</h1>
+      <ul className="flex">{li}</ul>
+
+      {/* 버전 3 */}
+      <hr />
+      <h1>기록된 숫자 v3</h1>
+      <ul className="flex">
+        {recordedNos.map((el, index) => (
+        <li key={index}>{el},</li>
+        ))}
+      </ul>
+
+
+
     </>
   );
 }
