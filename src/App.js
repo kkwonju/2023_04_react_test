5954b9ffffd0a7b35ba12d2ecf9831370b750e0b
kkwoju <kkwo4140@gmail.com>
2023-04-24 21:19:25 +0900

    Alert

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import { useState, useRef } from "react";
-import RefEx from "./RefEx";
+function Alert({color: _color, children}) {
+  const color = _color ?? "white";
+  return (
+    <div className="alert alert-success shadow-lg">
+      <div className={`text-${color}`}>
+        <span>
+          <i className="fa-solid fa-circle-info"></i>
+        </span>
+        <span>{children}</span>
+      </div>
+    </div>
+  )
+}
 
-export default function App() {
-
+function App() {
   return (
     <>
-      <RefEx />
+      <Alert>Hello</Alert>
+      <hr className="my-4" />
+      <Alert color="pink-500">Bye</Alert>
     </>
   );
 }
+
+export default App;
