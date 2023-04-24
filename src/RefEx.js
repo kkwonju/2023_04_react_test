a334452245eea36e04bec21bc693d1ccc03f93f2
kkwoju <kkwo4140@gmail.com>
2023-04-24 20:35:16 +0900

    배열에서 특정 요소 수정

--- src/RefEx.js
+++ src/RefEx.js
@@ -4,7 +4,17 @@ export default function RefEx() {
   const noInputRef = useRef(null);
   const [no, setNo] = useState("");
 
-  const [recordedNos, setRecordedNos] = useState([5, 10, 15, 20, 5, 20, 10, 5]);
+  const [recordedNos, setRecordedNos] = useState([
+    5,
+    10,
+    15,
+    20,
+    5,
+    25,
+    5,
+    30,
+    5
+  ]);
 
   const saveNo = () => {
     if (no === "") {
@@ -14,15 +24,18 @@ export default function RefEx() {
 
     setRecordedNos([...recordedNos, no]);
     setNo("");
+    noInputRef.current.focus();
   };
 
   const removeNo = (index) => {
-    const newRecordedNos = recordedNos.filter((el, _index) => _index != index);
+    const newRecordedNos = recordedNos.filter((_, _index) => _index != index);
+    setRecordedNos(newRecordedNos);
+  };
+
+  const modifyNo = (index, newNo) => {
+    const newRecordedNos = recordedNos.map((el, _index) => _index == index ? newNo : el);
     setRecordedNos(newRecordedNos);
   }
-
-
-  const li = recordedNos.map((el, index) => <li key={index}>{el}</li>);
 
   return (
     <>
@@ -34,21 +47,25 @@ export default function RefEx() {
       >
         <input
           type="number"
+          ref={noInputRef}
           value={no}
           onChange={(e) => setNo(e.target.valueAsNumber)}
         />
-        <button className="btnn" type="submit">기록</button>
+        <button type="submit">기록</button>
       </form>
 
       <hr />
 
-      <h1>기록된 숫자 v1</h1>
+      <h1>기록된 숫자</h1>
       <ul>
         {recordedNos.map((el, index) => (
           <li key={index}>
-            <span style={{ width: 50, display: "inline-block"}}>{el}</span>
-            <span style={{ width: 50, display: "inline-block"}}>{index}</span>
+            <span style={{ width: 70, display: 'inline-block' }}>{el}</span>
+            <span style={{ width: 70, display: 'inline-block' }}>{index}</span>
             <button className="btnn" onClick={() => removeNo(index)}>삭제</button>
+            <button className="btnn" onClick={() => modifyNo(index, el + 1)}>+1</button>
+            <button className="btnn" onClick={() => modifyNo(index, el - 1)}>-1</button>
+            <button className="btnn" onClick={() => modifyNo(index, 0)}>0</button>
           </li>
         ))}
       </ul>
