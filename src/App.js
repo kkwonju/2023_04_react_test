f7f391635bcd4e6150801f5b108da5a0251bf1db
kkwoju <kkwo4140@gmail.com>
2023-04-26 20:15:45 +0900

    useEffect 실행 경우의 수

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 
 let AppCallCount = 0;
 let SubCallCount = 0;
 
-function Sub() {
+function Sub({ appNo }) {
   SubCallCount++;
   console.log(`SubCallCount : ${SubCallCount}`);
 
   const [no, setNo] = useState(0);
+  const [no2, setNo2] = useState(0);
+
+  useEffect(() => {
+    console.log('effect 1 : 단 한 번 실행');
+  }, []);
+  
+  useEffect(() => {
+    console.log('effect 2 : 부모(App)의 appNo가 바뀔 때마다 실행');
+  }, [appNo]);
+
+  useEffect(() => {
+    console.log('effect 3 : 나(Sub)의 no가 바뀔 때마다 실행');
+  }, [no]);
+
+  useEffect(() => {
+    console.log('effect 4 : appNo 혹은 no가 변경될 때마다 실행');
+  }, [appNo, no]);
+
+  useEffect(() => {
+    console.log('effect 5 : 매번 실행');
+  });
 
   return (
     <>
       <div style={{ border: "10px solid blue", padding: 10 }}>
+        App no : {appNo}
         <button className="btn btn-outline" onClick={() => setNo(no + 1)}>
-          Sub 버튼 : {no}
+          Sub 버튼(No) : {no}
+        </button>
+        <button className="btn btn-outline" onClick={() => setNo2(no2 + 1)}>
+          Sub 버튼(No2) : {no2}
         </button>
       </div>
     </>
@@ -33,7 +58,7 @@ function App() {
           App 버튼 : {no}
         </button>
         <hr />
-        <Sub />
+        <Sub appNo={no}/>
       </div>
     </>
   );
