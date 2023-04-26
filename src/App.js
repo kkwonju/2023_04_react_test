7993b86467f1b585ce549efd77e92bd659f9afe2
kkwoju <kkwo4140@gmail.com>
2023-04-26 19:35:56 +0900

    useEffect 의존성 배열

--- src/App.js
+++ src/App.js
@@ -1,64 +1,24 @@
-import React, { useState } from "react";
-import classNames from 'https://cdn.skypack.dev/classnames';
+import React, { useEffect, useState, useRef } from "react";
 
-function NotifyOnce({ children }) {
-  const [visible, setVisible] = useState(false);
-  const [wordDone, setWorkDone] = useState(false);
-
-  if(wordDone == false){
-    setTimeout(function() {
-      setVisible(true);
-    }, 1000);
-
-    setTimeout(function() {
-      setVisible(false);
-    }, 3000);
-
-    setWorkDone(true);
-  }
-  return (
-    <>
-      <div className={classNames(
-        "fixed transition-all right-[10px]",
-        {
-          "top-[-60px]": !visible,
-        },
-        {
-          "top-[10px]": visible,
-        }
-      )}
-      >
-        {children}
-      </div>
-    </>
-  )
-}
-
-function Alert({ color: _color, children }) {
-  const color = _color ?? "white";
-  return (
-    <div className="alert alert-success shadow-lg">
-      <div className={`text-${color}`}>
-        <span>
-          <i className="fa-solid fa-circle-info"></i>
-        </span>
-        <span>{children}</span>
-      </div>
-    </div>
-  )
-}
+let AppCallCount = 0;
 
 function App() {
+  const inputNameRef = useRef(null);
+  const inputAgeRef = useRef(null);
+
+  console.log("시작");
+
+  useEffect(() => {
+    AppCallCount++;
+    console.log(`AppCallCount : ${AppCallCount}번`);
+  }, [])
+
+  console.log("끝");
+
+  const [no, setNo] = useState(0);
   return (
     <>
-      <NotifyOnce>
-        <Alert>안녕하세요</Alert>
-      </NotifyOnce>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas facere nulla sint
-        voluptates perspiciatis est doloremque incidunt exercitationem, unde amet quasi corporis
-        aperiam nihil excepturi, quaerat nesciunt. Fugiat, reiciendis modi!
-      </div>
+      <button onClick={() => setNo(no + 1)}>증가 : {no}</button>
     </>
   );
 }
