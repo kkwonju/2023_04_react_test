748b67674278dc8ded4461c97e0f96b2f6902c21
kkwoju <kkwo4140@gmail.com>
2023-04-26 21:19:12 +0900

    dark모드

--- src/App.js
+++ src/App.js
@@ -1,64 +1,40 @@
 import React, { useEffect, useState } from "react";
 
 let AppCallCount = 0;
-let SubCallCount = 0;
-
-function Sub({ appNo }) {
-  SubCallCount++;
-  console.log(`SubCallCount : ${SubCallCount}`);
-
-  const [no, setNo] = useState(0);
-  const [no2, setNo2] = useState(0);
-
-  useEffect(() => {
-    console.log('effect 1 : 단 한 번 실행');
-  }, []);
-  
-  useEffect(() => {
-    console.log('effect 2 : 부모(App)의 appNo가 바뀔 때마다 실행');
-  }, [appNo]);
-
-  useEffect(() => {
-    console.log('effect 3 : 나(Sub)의 no가 바뀔 때마다 실행');
-  }, [no]);
-
-  useEffect(() => {
-    console.log('effect 4 : appNo 혹은 no가 변경될 때마다 실행');
-  }, [appNo, no]);
-
-  useEffect(() => {
-    console.log('effect 5 : 매번 실행');
-  });
-
-  return (
-    <>
-      <div style={{ border: "10px solid blue", padding: 10 }}>
-        App no : {appNo}
-        <button className="btn btn-outline" onClick={() => setNo(no + 1)}>
-          Sub 버튼(No) : {no}
-        </button>
-        <button className="btn btn-outline" onClick={() => setNo2(no2 + 1)}>
-          Sub 버튼(No2) : {no2}
-        </button>
-      </div>
-    </>
-  );
-}
 
 function App() {
   AppCallCount++;
   console.log(`AppCallCount : ${AppCallCount}`);
 
   const [no, setNo] = useState(0);
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    const html = document.getElementsByTagName("html")[0];
+
+    if (isDark) {
+      html.classList.add("dark");
+    } else {
+      html.classList.remove("dark");
+    }
+  }, [isDark]);
 
   return (
     <>
-      <div style={{ border: "10px solid red", padding: 10 }}>
+      <div>
         <button className="btn btn-outline" onClick={() => setNo(no + 1)}>
           App 버튼 : {no}
         </button>
+        <button className="btn btn-outline" onClick={() => setIsDark(!isDark)}>
+          테마 토글
+        </button>
+
         <hr />
-        <Sub appNo={no}/>
+
+        <div>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam voluptate ut illum esse optio fuga, aut quos voluptas fugiat maxime, eveniet laboriosam eius harum reiciendis iure incidunt similique quis deleniti.
+        </div>
+        <h1 className="color-primary">Hello, Nice to meet you</h1>
       </div>
     </>
   );
