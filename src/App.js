ef49f04aa5e1fef3b5c1b63d5fe99bb09329c8a7
kkwoju <kkwo4140@gmail.com>
2023-04-26 19:52:07 +0900

    부모 컴포넌트를 실행했을 때 자식 컴포넌트에 영향

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { useEffect, useState, useRef } from "react";
 
 let AppCallCount = 0;
+let SubCallCount = 0;
 
-function App() {
-  const inputNameRef = useRef(null);
-  const inputAgeRef = useRef(null);
+function Sub() {
+  SubCallCount++;
+  console.log(`SubCallCount : ${SubCallCount}`);
+
   const [no, setNo] = useState(0);
-
-  useEffect(() => {
-    inputNameRef.current.focus();
-  }, [])
 
   return (
     <>
-      <input ref={inputNameRef} type="text" placeholder="이름" />
-      <hr />
-      <input ref={inputAgeRef} type="number" placeholder="나이" />
-      <hr />
-      <button onClick={() => { setNo(no + 1); inputAgeRef.current.focus(); }}>
-        증가 : {no}
-      </button>
+      <div style={{ border: "10px solid blue", padding: 10 }}>
+        <button className="btn btn-outline" onClick={() => setNo(no + 1)}>
+          Sub 버튼 : {no}
+        </button>
+      </div>
+    </>
+  );
+}
+
+function App() {
+  AppCallCount++;
+  console.log(`AppCallCount : ${AppCallCount}`);
+
+  const [no, setNo] = useState(0);
+
+  return (
+    <>
+      <div style={{ border: "10px solid red", padding: 10 }}>
+        <button className="btn btn-outline" onClick={() => setNo(no + 1)}>
+          App 버튼 : {no}
+        </button>
+        <hr />
+        <Sub />
+      </div>
     </>
   );
 }
