56a23a3060649dcd9b71aeaa8f92d4e0bbab6a7a
kkwoju <kkwo4140@gmail.com>
2023-04-28 19:52:29 +0900

    useMemo를 하기 위한 준비, 1부터 입력값까지의 소수개수를 알려주는 프로그램 구현에 useEffect 적용

--- src/App.js
+++ src/App.js
@@ -1,41 +1,86 @@
 import React, { useEffect, useState } from "react";
 
-let AppCallCount = 0;
+// 
+function isPrimeNumber(no) {
+  for (let i = 2; i < no; i++) {
+    if (i * i > no) {
+      break;
+    }
 
+    if (no % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function getPrimeNumbers(max) {
+  const primeNumbers = [];
+
+  for (let i = 2; i < max; i++) {
+    if (isPrimeNumber(i)) {
+      primeNumbers.push(i);
+    }
+  }
+  return primeNumbers;
+}
+
+function getPrimeNumbersCount(max) {
+  return getPrimeNumbers(max).length;
+}
+
+// App
 function App() {
-  AppCallCount++;
-  console.log(`AppCallCount : ${AppCallCount}`);
-
+  const [inputedNo, setInputedNo] = useState(0);
   const [no, setNo] = useState(0);
-  const [isDark, setIsDark] = useState(false);
+  
+  const [primeNumbersCount, setPrimeNumbersCount] = useState(0);
 
   useEffect(() => {
-    const html = document.getElementsByTagName("html")[0];
+    const primeNumbersCount = getPrimeNumbersCount(inputedNo);
+    setPrimeNumbersCount(primeNumbersCount);
+  }, [inputedNo]);
+  
+  // const primeNumbersCount = getPrimeNumbersCount(inputedNo);
 
-    if (isDark) {
-      html.classList.add("dark");
-    } else {
-      html.classList.remove("dark");
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    form.number.value = form.number.value.trim();
+
+    if (form.number.value.length == 0) {
+      alert('숫자를 입력하세요!');
+      form.number.focus();
+
+      return;
     }
-  }, [isDark]);
+
+    const number = form.number.valueAsNumber;
+    form.number.focus();
+
+    setPrimeNumbersCount(primeNumbersCount);
+    setInputedNo(number);
+  }
 
   return (
     <>
-      <div>
-        <button className="btn btn-outline" onClick={() => setNo(no + 1)}>
-          App 버튼 : {no}
-        </button>
-        <button className="btn btn-outline" onClick={() => setIsDark(!isDark)}>
-          테마 토글
-        </button>
-
+    <button onClick={() => setNo(no + 1)}> 번호 : {no} </button>
+      <hr />
+      <form onSubmit={onSubmit}>
+        <input
+          type="number"
+          name="number"
+          placeholder="숫자를 입력해주세요"
+          defaultValue="0"
+          className="input input-bordered" />
+        <button type="submit" className="btn btn-outline">확인</button>
         <hr />
-
-        <div>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam voluptate ut illum esse optio fuga, aut quos voluptas fugiat maxime, eveniet laboriosam eius harum reiciendis iure incidunt similique quis deleniti.
-        </div>
-        <h1 className="color-primary">Hello, Nice to meet you</h1>
-      </div>
+        <div>MAX : {inputedNo}</div>
+        <div>소수의 갯수 : {primeNumbersCount}</div>
+      </form>
     </>
   );
 }
