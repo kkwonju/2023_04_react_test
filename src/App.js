522e4ebe1f92db87487ae7184667e7cdc1f6b7db
kkwoju <kkwo4140@gmail.com>
2023-05-01 21:44:34 +0900

    여러가지 선택 기능_2, 라디오 버튼 활용

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import Order from "./Order";
 
 function App() {
   const fruits = ["사과", "바나나", "딸기"];
+  const [selectedGender, setSelectedGender] = useState("M");
+
+
   const [selecteds, setSelecteds] = useState(new Array(fruits.length).fill(true));
   const selectedsFruits = selecteds.map((el, index) => el ? fruits[index] : el).filter((el) => el);
+
   const toggleFruitSelected = (index) => {
     const newSelecteds = selecteds.map((el, _index) =>
       _index == index ? !el : el);
@@ -27,14 +31,33 @@ function App() {
           </li>
         ))}
       </ul>
-      
+
       <div>
         선택 상태 : {selecteds.join(',')}
         <hr />
         선택 과일 : {selectedsFruits.join(',')}
       </div>
+      <hr />
 
+      <h2>라디오 버튼</h2>
+      <label>
+        <input
+          type="radio"
+          name="gender"
+          onChange={(e) => setSelectedGender("M")}
+          checked={selectedGender == "M"} />
+        남성
+      </label>
 
+      <label>
+        <input
+          type="radio"
+          name="gender"
+          onChange={(e) => setSelectedGender("W")}
+          checked={selectedGender == "W"} />
+        여성
+      </label>
+      <div>현재 값 : {selectedGender}</div>
       {/* <Order/> */}
     </>
   );
