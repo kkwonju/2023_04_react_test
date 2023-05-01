51b4010210420f2cfb4a57d964aae31c540f1a30
kkwoju <kkwo4140@gmail.com>
2023-05-01 21:35:34 +0900

    여러가지 선택 기능_1, 선택에 따른 true, false 표시, 선택된 과일 이름 표시

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Order from "./Order";
 
 function App() {
+  const fruits = ["사과", "바나나", "딸기"];
+  const [selecteds, setSelecteds] = useState(new Array(fruits.length).fill(true));
+  const selectedsFruits = selecteds.map((el, index) => el ? fruits[index] : el).filter((el) => el);
+  const toggleFruitSelected = (index) => {
+    const newSelecteds = selecteds.map((el, _index) =>
+      _index == index ? !el : el);
+    setSelecteds(newSelecteds);
+  }
 
   return (
     <>
-      <Order/>
+      <ul>
+        {fruits.map((fruit, index) => (
+          <li key={index}>
+            <label>
+              <input
+                type="checkbox"
+                onChange={() => toggleFruitSelected(index)}
+                checked={selecteds[index]}
+              />
+              {fruit}
+            </label>
+          </li>
+        ))}
+      </ul>
+      
+      <div>
+        선택 상태 : {selecteds.join(',')}
+        <hr />
+        선택 과일 : {selectedsFruits.join(',')}
+      </div>
+
+
+      {/* <Order/> */}
     </>
   );
 };
