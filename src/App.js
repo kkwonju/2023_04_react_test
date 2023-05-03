e9ddf6ef5d590e7ffa7f39fb44992e094cd770fa
kkwoju <kkwo4140@gmail.com>
2023-05-03 19:50:28 +0900

    함수로 만들어 관리

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React, { useState } from "react";
-import Order from "./Order";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  /** 실제 실행될 기능 */
+  const addTodo = (newTodo) => {
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+  }
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, _index) => _index != index);
+    setTodos(newTodos);
+  }
+
+  const modifyTodo = (index, newTodo) => {
+    const newTodos = todos.map((todo, _index) => _index != index ? todo : newTodo);
+    setTodos(newTodos);
+  }
+
+  /** 클릭 시 실행 */
+  const onAddBtnClick = () => {
+    addTodo(todos.length + 1);
+  }
+
+  const onRemoveBtnClick = () => {
+    removeTodo(0);
+  }
+
+  const onEditBtnClick = () => {
+    modifyTodo(1, "안녕");
+  }
+
+  // const onClick = () => {
+  //   // const newTodos = [...todos, todos.length + 1];
+  //   // setTodos(newTodos);
+  //   addTodo(todos.length + 1);
+  // }
 
   return (
     <>
-      <Order/>
+      <div>
+        {JSON.stringify(todos)}
+      </div>
+      <button onClick={onAddBtnClick}>추가</button>
+      <button onClick={onRemoveBtnClick}>제거</button>
+      <button onClick={onEditBtnClick}>수정</button>
     </>
   );
 };
