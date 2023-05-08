8dcb85fc8b6642f6d5383ea0fce1b326c4106dd7
kkwoju <kkwo4140@gmail.com>
2023-05-08 20:44:29 +0900

    추가, 수정, 삭제 구현, 수정 모드와 입력 폼 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React, { useState, useRef } from "react";
 function NewTodoForm({ todosState }) {
   const onSubmit = (e) => {
     e.preventDefault();
-    
+
     const form = e.target;
-    
+
     form.content.value = form.content.value.trim();
 
-    if(form.content.value.length == 0){
+    if (form.content.value.length == 0) {
       alert("뭐라도 입력해..");
       form.content.focus();
       return;
@@ -20,32 +20,90 @@ function NewTodoForm({ todosState }) {
   return (
     <>
       <form onSubmit={onSubmit}>
-        <input autoComplete="off" type="text" name="content" placeholder="할 일을 입력하세요"/>
-        <input type="submit" value="추가"/>
-        <input type="reset" value="취소"/>
+        <input autoComplete="off" type="text" name="content" placeholder="할 일을 입력하세요" />
+        <input type="submit" value="추가" />
+        <input type="reset" value="취소" />
       </form>
     </>
   )
 }
 
-// 보여주는 역할
+// 리스트( 메뉴 ) 아이템
+function TodoListItem({ todosState, todo, index }) {
+  const [editMode, setEditMode] = useState(false);
+  const [editedContent, setEditContent] = useState(todo.content);
+  const editedContentInputRef = useRef(null);
+
+  const removeTodo = () => {
+    todosState.removeTodo(index);
+  };
+
+  const showEdit = () => {
+    setEditMode(true);
+  }
+
+  const commitEdit = () => {
+    if(editedContent.trim().length == 0){
+      alert('할 일 입력해..');
+      editedContentInputRef.current.focus();
+      return;
+    }
+
+    todosState.modifyTodo(index, editedContent.trim());
+
+    setEditMode(false);
+  }
+
+  const cancelEdit = () => {
+    setEditMode(false);
+    setEditContent(todo.content);
+  }
+
+  return (
+    <li key={index}>
+      {todo.id} {todo.regDate}
+      {editMode || (
+        <>
+           {todo.content}
+          &nbsp;
+          <button onClick={showEdit}>수정</button>{" "}
+        </>
+      )}
+      {editMode && (
+        <>
+          <input type="text" placeholder="할 일 입력" value={editedContent} onChange={(e) => setEditContent(e.target.value)}/>
+          <button onClick={commitEdit}>수정완료</button>
+          <button onClick={cancelEdit}>수정취소</button>
+        </>
+      )}
+      <button onClick={removeTodo}>삭제</button>
+    </li>
+  )
+}
+
+// 리스트( 메뉴 )
+function TodoList({ todosState }) {
+  return (
+    <ul>
+      {todosState.todos.map((todo, index) => (
+        <TodoListItem todosState={todosState} key={todo.id} todo={todo} index={index} />
+      ))}
+    </ul>
+  )
+}
+
+// 할 일 App
 function TodoApp({ todosState }) {
   return (
     <>
       <NewTodoForm todosState={todosState} />
       <hr />
-      <ul>
-        {todosState.todos.map((todo, index) => (
-          <li key={index}>
-            {todo.id} {todo.regDate} {todo.content}
-          </li>
-        ))}
-      </ul>
+      <TodoList todosState={todosState} />
     </>
   )
 }
 
-//
+// 기능
 function useTodoState() {
   const [todos, setTodos] = useState([]);
   const lastTodoIdRef = useRef(0);
@@ -81,6 +139,7 @@ function useTodoState() {
   };
 };
 
+// App
 function App() {
   const todosState = useTodoState();
   return (
